84bffeb3a554ae765ec50f3de0e4424baf8552e1
Parker Mitchell <richard.parker.mitchell@gmail.com>
2021-08-24 20:40:30 -0700

    added better comments/JSDocs for reservation validation

--- front-end/src/validation/reservations.js
+++ front-end/src/validation/reservations.js
@@ -3,15 +3,25 @@ import { getDateFromReso } from "../utils/date-time";
 /**
  *
  * @param {object} reservationInfo
- * @returns
+ * @returns {String[]}
+ * An array of strings, representing validation errors specific to date and time
  */
 function getDateAndTimeValidationErrors(reservationInfo) {
   //Return early if we are missing our date and time values
   if (!reservationInfo.reservation_date || !reservationInfo.reservation_time)
     return [];
   const errors = [];
-  const now = new Date();
   const reso_date = getDateFromReso(reservationInfo);
+
+  const now = new Date();
+  if (reso_date < now) {
+    errors.push("Cannot make reservations in the past.");
+  }
+  if (reso_date.getDay() === 2) {
+    errors.push("Cannot make reservations on Tuesday. Restaurant is closed.");
+  }
+
+  //Time the restaurant opens on reservation date
   const openTime = new Date(
     reso_date.getFullYear(),
     reso_date.getMonth(),
@@ -19,6 +29,13 @@ function getDateAndTimeValidationErrors(reservationInfo) {
     10,
     30
   );
+  if (reso_date < openTime) {
+    errors.push(
+      "Cannot make reservation before 10:30 AM when restaurant opens."
+    );
+  }
+
+  //30 minutes before time the restaurant closes on reservation date
   const closeTime = new Date(
     reso_date.getFullYear(),
     reso_date.getMonth(),
@@ -26,24 +43,6 @@ function getDateAndTimeValidationErrors(reservationInfo) {
     21,
     30
   );
-  // console.log(
-  //   "reso",
-  //   reso_date,
-  //   // "open", openTime,
-  //   "close",
-  //   closeTime
-  // );
-  if (reso_date < now) {
-    errors.push("Cannot make reservations in the past.");
-  }
-  if (reso_date.getDay() === 2) {
-    errors.push("Cannot make reservations on Tuesday. Restaurant is closed.");
-  }
-  if (reso_date < openTime) {
-    errors.push(
-      "Cannot make reservation before 10:30 AM when restaurant opens."
-    );
-  }
   if (reso_date > closeTime) {
     errors.push(
       "Cannot make reservation after 9:30PM as restaurant closes at 10."
@@ -56,7 +55,8 @@ function getDateAndTimeValidationErrors(reservationInfo) {
 /**
  * Gets all missing properties that are required for a reservation. This is not exported as it is not used outside of this file.
  * @param {object} reservationInfo
- * @returns array
+ * @returns {array}
+ * Array of strings, representing errors for missing properties
  */
 function getMissingProperties(reservationInfo) {
   const missingProperties = [];
@@ -86,12 +86,12 @@ function getMissingProperties(reservationInfo) {
 /**
  * Check if reservationInfo contains any validation errors such as missing parameters or invalid dates/times
  * @param {object} reservationInfo
- * @returns array
+ * @returns {array}
+ * Array of strings, representing all validation errors for the reservation
  */
 export function getValidationErrors(reservationInfo) {
   if (!reservationInfo)
     throw new Error("Must pass reservation information to be validated.");
-  // console.log("lets validate");
   let validationErrors = [];
   validationErrors = validationErrors.concat(
     getMissingProperties(reservationInfo)
@@ -99,6 +99,6 @@ export function getValidationErrors(reservationInfo) {
   validationErrors = validationErrors.concat(
     getDateAndTimeValidationErrors(reservationInfo)
   );
-  // console.log(validationErrors);
+  if (validationErrors.length > 0) console.error(validationErrors);
   return validationErrors;
 }
